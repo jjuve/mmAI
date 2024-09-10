c46c0bbacaf3cf2b4279516c2713c99a0953517d
jjuve <75339345+jjuve@users.noreply.github.com>
2024-09-09 20:29:14 -0500

    started using playwright

--- scraper.py
+++ scraper.py
@@ -1,3 +1,11 @@
-from bs4 import BeautifulSoup
-import requests
+from playwright.sync_api import sync_playwright, Playwright
 
+def run(playwright: Playwright):
+    chromium = playwright.chromium
+    browser = chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.espn.com/college-football/scoreboard")
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
