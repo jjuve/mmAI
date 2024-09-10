798cd2b9593da05c7af89313e53a3c7c18622236
jjuve <75339345+jjuve@users.noreply.github.com>
2024-09-09 21:15:46 -0500

    change to scraper function

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 from playwright.sync_api import sync_playwright, Playwright
 
-def run(playwright: Playwright):
-    chromium = playwright.chromium
-    browser = chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.espn.com/college-football/scoreboard")
-    browser.close()
-
-with sync_playwright() as playwright:
-    run(playwright)
+def scrape_scores():
+      with sync_playwright() as p:
+             browser = p.chromium.launch(headless=False)
+             page = browser.new_page()
+             page.goto("https://www.espn.com/college-football/scoreboard")
+             
+             browser.close()
+          
+          
+if __name__ == "__main__":
+	scrape_scores()
