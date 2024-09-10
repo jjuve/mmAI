e6643db134fc330e8f47003c502534c3aacde43e
jjuve <75339345+jjuve@users.noreply.github.com>
2024-09-09 22:09:14 -0500

    returns scores html

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
 from playwright.sync_api import sync_playwright, Playwright
+from bs4 import BeautifulSoup
 
 def scrape_scores():
       with sync_playwright() as p:
              browser = p.chromium.launch(headless=False)
              page = browser.new_page()
-             page.goto("https://www.espn.com/college-football/scoreboard")
-             
+             page.goto("https://www.espn.com/college-football/scoreboard/_/week/1/year/2024/seasontype/2")
+             html_content = page.content()
+             soup = BeautifulSoup(html_content, "html.parser")
+             print(soup.prettify())
              browser.close()
           
           
