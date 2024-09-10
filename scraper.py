f07acd33f66c2673371c81757df7b20eb12999e4
jjuve <75339345+jjuve@users.noreply.github.com>
2024-09-10 17:03:56 -0500

    scraped both team names and scores

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright, Playwright
 from bs4 import BeautifulSoup
-import json
+import json, time
 
 def scrape_scores():
       with sync_playwright() as p:
@@ -10,6 +10,14 @@ def scrape_scores():
              html_content = page.content()
              soup = BeautifulSoup(html_content, "html.parser")
              print(soup.find('h1').get_text())
+             teams = soup.find_all('div', class_='ScoreCell__TeamName')
+             scores = soup.find_all('div', class_='ScoreCell__Score')
+             for team in teams:
+                    #print(team.get_text())
+                    for score in scores:
+                           print(team.get_text())
+                           print(score.get_text())
+                           break
              
              browser.close()
           
