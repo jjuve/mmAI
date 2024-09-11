b4d89128ccf43d0b0518e78de18ff20b1e3c8729
jjuve <75339345+jjuve@users.noreply.github.com>
2024-09-10 21:34:34 -0500

    changed scraper function to class to add more functionality

--- scraper.py
+++ scraper.py
@@ -1,26 +1,38 @@
 from playwright.sync_api import sync_playwright, Playwright
 from bs4 import BeautifulSoup
-import json, time
 
-def scrape_scores():
-      with sync_playwright() as p:
-             browser = p.chromium.launch(headless=False)
-             page = browser.new_page()
-             page.goto("https://www.espn.com/college-football/scoreboard/_/week/1/year/2024/seasontype/2")
-             html_content = page.content()
-             soup = BeautifulSoup(html_content, "html.parser")
-             print(soup.find('h1').get_text())
-             teams = soup.find_all('div', class_='ScoreCell__TeamName')
-             scores = soup.find_all('div', class_='ScoreCell__Score')
-             for team in teams:
-                    #print(team.get_text())
-                    for score in scores:
-                           print(team.get_text())
-                           print(score.get_text())
-                           break
-             
-             browser.close()
+class ESPNscraper:
+       def __init__(self):
+              self.browser = None
+              self.page = None
+
+       def start_browser(self, headless = True):
+                  self.browser = p.chromium.launch(headless=headless)
+                  self.page = self.browser.new_page()
+       
+       def scrape_scores(self, url = "https://www.espn.com/college-football/scoreboard/_/week/1/year/2024/seasontype/2"):
+              self.page.goto(url)
+              html_content = self.page.content()
+              soup = BeautifulSoup(html_content, "html.parser")
+
+              print(soup.find('h1').get_text())
+              teams = soup.find_all('div', class_='ScoreCell__TeamName')
+              scores = soup.find_all('div', class_='ScoreCell__Score')
+
+              team_names = [team.get_text() for team in teams]
+              score_values = [score.get_text() for score in scores]
+
+              return team_names, score_values
+       
+       def close_browser(self):
+              if self.browser:
+                     self.browser.close()
           
           
 if __name__ == "__main__":
-	scrape_scores()
+      with sync_playwright() as p:
+            scraper = ESPNscraper()
+            scraper.start_browser(headless=True)
+            teams, scores= scraper.scrape_scores()
+            print(teams, scores)
+            scraper.close_browser()
